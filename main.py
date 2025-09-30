0453bdbe65d9e362f9a27504de56378865874e3c
marexxxxxxx <marecsalzm@gmail.com>
2025-09-30 16:22:34 +0200

    working knowledgegraph creater

--- main.py
+++ main.py
@@ -2,15 +2,18 @@ from llama_index.graph_stores.memgraph import MemgraphPropertyGraphStore
 from llama_index.llms.ollama import Ollama
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.core import SimpleDirectoryReader
-
+from llama_index.core import Settings
 import logging
 import sys
+import nest_asyncio
+nest_asyncio.apply()
 
 
-
-llm_model = Ollama(model="gpt-oss:20b", request_timeout=460.0)
+llm_model = Ollama(model="hf.co/unsloth/gpt-oss-20b-GGUF:Q8_0", request_timeout=460.0)
 emebdder = Ollama(model="")
 doc = SimpleDirectoryReader("./data/").load_data()
+
+Settings.llm = llm_model
 
 def main():
     username = ""  # Your Memgraph username, default is ""
@@ -25,17 +28,16 @@ def main():
     
     from llama_index.core import PropertyGraphIndex
     from llama_index.embeddings.huggingface import HuggingFaceEmbedding
-    from llama_index.core.indices.property_graph import SchemaLLMPathExtractor
+    from llama_index.core.indices.property_graph import SchemaLLMPathExtractor, DynamicLLMPathExtractor
 
-    # Embedding-Modell: BAAI/bge-m3
     embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-m3")
-
-    # LLM: gpt-oss20b
 
     index = PropertyGraphIndex.from_documents(
         doc,
         embed_model=embed_model,
-        kg_extractors=[
+        kg_extractors=[DynamicLLMPathExtractor(
+            llm=llm_model
+        ),
             SchemaLLMPathExtractor(
                 llm=llm_model,
             )
@@ -43,12 +45,6 @@ def main():
         property_graph_store=graph_store,
         show_progress=True,
     )
-    query_engine = index.as_query_engine(include_text=True)
-
-    response = query_engine.query("Who did Charles Robert Darwin collaborate with?")
-    print(str(response))
-
-
 
 
 if __name__ == "__main__":
